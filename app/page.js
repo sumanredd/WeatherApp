d0a5b4e42a0a62f53bd4323cb3440aaa35a4e78e
Sumanth Reddy <121952320+sumanredd@users.noreply.github.com>
2025-09-02 22:40:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,24 @@
 'use client'
 
+import { Suspense } from "react";
 import Weather from "./components/Weather";
 
 export default function Page() {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      minHeight: "100vh",
-      background: "#121212",
-      color: "#fff"
-    }}>
-      <Weather />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        background: "#121212",
+        color: "#fff"
+      }}
+    >
+      <Suspense fallback={<div>Loading weather...</div>}>
+        <Weather />
+      </Suspense>
     </div>
   );
 }
